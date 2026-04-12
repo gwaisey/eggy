7e59ecb927b8a64a9e050266c9db62a5e11760e3
gwaisey <gracemaegozali@gmail.com>
2026-04-13 00:48:54 +0700

    feat: add vercel deployment config and build script

---
+++ vercel_build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 1. Clone Flutter SDK (Depth 1 for speed)
+git clone https://github.com/flutter/flutter.git -b stable --depth 1
+
+# 2. Set Path
+export PATH="$PATH:`pwd`/flutter/bin"
+
+# 3. Disable Analytics & Setup
+flutter config --no-analytics
+
+# 4. Build for Web
+flutter build web --release
